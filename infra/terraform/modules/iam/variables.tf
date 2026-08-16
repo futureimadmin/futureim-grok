8f031f40ddec41db06bcaf587b17a0c82b9f512a
futureimadmin <anantha.kommaraju@futureim.in>
2026-08-16 22:45:39 +0530

    infra: IAM module variables

---
+++ infra/terraform/modules/iam/variables.tf
@@ -0,0 +1,8 @@
+variable "project_id" {
+  type = string
+}
+
+variable "project_number" {
+  description = "Numeric project number (needed for serverless robot SA and Eventarc SA)"
+  type        = string
+}
