a7192b8eceb8f2911c8435d4132bbafee4e8554c
futureimadmin <anantha.kommaraju@futureim.in>
2026-08-16 22:35:22 +0530

    infra: dev outputs

---
+++ infra/terraform/environments/dev/outputs.tf
@@ -0,0 +1,32 @@
+output "vpc_network" {
+  value = module.vpc.network_name
+}
+
+output "subnet_cidrs" {
+  value = module.vpc.subnet_cidrs
+}
+
+output "documents_bucket" {
+  value = module.services.documents_bucket_name
+}
+
+output "redis_host" {
+  value     = module.services.redis_host
+  sensitive = true
+}
+
+output "ingestion_sa" {
+  value = module.iam.ingestion_sa_email
+}
+
+output "serving_sa" {
+  value = module.iam.serving_sa_email
+}
+
+output "vpc_access_connector" {
+  value = google_vpc_access_connector.rag_connector.id
+}
+
+output "artifact_registry" {
+  value = module.services.artifact_registry_url
+}
